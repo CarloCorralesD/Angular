97824b4b20065d4ac0699142499d82c073292039
Carlo Corrales D <ccorrales@unsa.edu.pe>
2020-07-13 08:49:12 -0500

    Todo Instalado, ejemplo inicial y Uso de For e Ifs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+//import { NgModule } from '@angular/core';
+//import { FormsModule } from '@angular/forms';
+
+//import {PostService} from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //providers: [PostService]
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name : string;
+  email; // : string ;
+  webpage : string;
+  hobbies : string[];
+  showHobbies : boolean;
+  
+  constructor() { //private postService: PostService) {
+  	console.log("Constructor working...");
+  	this.name = "Carlo Jose Luis";
+  	this.email = "ccorrales@unsa.edu.pe";
+  	this.webpage = "http://www.unsa.edu.pe";
+  	this.hobbies = ["Futbol","Programación","Netflix"];
+  	this.showHobbies = false;
+  	/* this.postService.getPosts().subscribe(posts=> {
+  		console.log(posts);
+  	}); */
+  }
+
+  toggleHobbies() {
+  	this.showHobbies = !this.showHobbies;
+  }
+
+  newHobby(hobby) {
+  	//console.log(hobby.value);
+  	this.hobbies.push(hobby.value);
+  	hobby.value = "";
+  	return false;
+  }
 }
+
+/*@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+
+export class AppModule { }. */
